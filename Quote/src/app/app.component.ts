447704b58030d36b39c577918935971f2286417c
lydia-254 <lydia.wangare@student.moringaschool.com>
2021-05-18 11:01:03 +0300

    Update Quote

--- Quote/src/app/app.component.ts
+++ Quote/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quote';
+  quote:Quote[] = [
+    {quote:'the purpose of life is to be happy',author:'Kangeri Lydia',name:'Lydia'},
+    {quote:'if you find someone that you love in your life, then hang on to that love.',author:'Princess Diana',name:'Diana'},
+    {quote:'intellectual growth should commence at birth and ease only at death.',author:'Albert Eistein',name:'Eistein'},
+  ];
 }
